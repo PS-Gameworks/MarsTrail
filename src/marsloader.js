01030885e128ea12db4ddef5412e08dbb8f011ba
David Fondren <david.h.fondren@gmail.com>
2014-06-02 21:23:28 -0400

    Menu and Game Window Updates
    - Centered game window using iframe.
    - Added background graphic for menu screen.

--- src/marsloader.js
+++ src/marsloader.js
@@ -2,10 +2,12 @@ var loadState = new Kiwi.State('loadState');
 
 loadState.preload = function(){
     Kiwi.State.prototype.preload.call(this);
-  	this.addImage('mars','img/mars.png');
+    this.addImage('mars','img/mars.png');
+    this.addImage('mars_menu_bg','img/mars_menu_bg.jpg');
+    this.game.stage.resize(900, 600);
 }
 
 loadState.create = function(){
     Kiwi.State.prototype.create.call(this);
-	this.game.states.switchState("introState");
+    this.game.states.switchState("introState");
 }
--- src/menu.js
+++ src/menu.js
@@ -1,16 +1,20 @@
 var menuState = new Kiwi.State('menuState');
 
-menuState.create = function(){
+menuState.create = function() {
     Kiwi.State.prototype.create.call(this);
-	this.mouse = this.game.input.mouse;
-	this.message = new Kiwi.GameObjects.Textfield(this, "Welcome to Main Menu -- Click here to continue!", 0, 0);
-	this.addChild(this.message);
+    this.mouse = this.game.input.mouse;
+    this.background = new Kiwi.GameObjects.StaticImage(this, this.textures['mars_menu_bg'], 0, 0);
+    this.title = new Kiwi.GameObjects.Textfield(this, "THE MARS TRAIL", 150, 150, '#ffffff', 75, "bold");
+    this.newGameText = new Kiwi.GameObjects.Textfield(this, "New Game", 380, 300, '#ffffff', 30, "bold");
+    this.addChild(this.background);
+    this.addChild(this.title);
+    this.addChild(this.newGameText);
 }
 
-menuState.update = function(){
-   Kiwi.State.prototype.update.call(this);
-   if (this.mouse.isDown) {
-		this.mouse.reset();
-		this.game.states.switchState("travelState");
-   }
+menuState.update = function() {
+    Kiwi.State.prototype.update.call(this);
+    if (this.mouse.isDown) {
+        this.mouse.reset();
+        this.game.states.switchState("travelState");
+    }
 };
